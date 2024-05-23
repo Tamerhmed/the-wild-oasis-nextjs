0e2b238782e4c21be064d4d61bda32272a650703
Tamer Ahmed <tamer30230@gmail.com>
2024-05-23 19:49:00 +1000

    fetch data from supabase

---
+++ app/account/page.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Profile",
+};
+
+export default function AccountPage() {
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Jonas
+      </h2>
+    </div>
+  );
+}
